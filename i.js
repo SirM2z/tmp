4de273a5512209bb1c7a28778a7601146da67613
sirm2z <m18367819165@gmail.com>
2018-05-03 10:50:33 +0800

    update

--- i.js
+++ i.js
@@ -1,4 +1,3 @@
-const Promise = require('es6-promise').Promise
 const axios = require('axios')
 const cheerio = require('cheerio')
 const fs = require('fs')
@@ -9,9 +8,9 @@ function writeLog (content) {
   })
 }
 
-const rootUrl = 'http://blog.csdn.net'
 const stratUrl = 'http://blog.csdn.net/sirm2z'
-const listUrl = 'http://blog.csdn.net/sirm2z/svc/getarticles?pageindex=1&pagesize=100&categoryId=0'
+const listUrl2 = 'https://blog.csdn.net/sirm2z/article/list/2'
+const listUrl3 = 'https://blog.csdn.net/sirm2z/article/list/3'
 let startViews = null
 let endViews = null
 let blogNum = 0
@@ -21,19 +20,24 @@ let errNum = 0
 async function start() {
   let res = await axios.get(stratUrl)
   let $ = cheerio.load(res.data)
-  startViews = $('.gradeAndbadge .num')[0].children[0].data
+  startViews = $('.grade-box dd')[1].attribs.title
   console.log(`当前访问量：${startViews}`)
   console.log(`---------------------------------------`)
-  let listRes = await axios.get(listUrl)
-  let list$ = cheerio.load(listRes.data)
-  const blogList = list$('.blog-unit')
+  // let blogList = $('.article-item-box')
+  let listRes2 = await axios.get(listUrl2)
+  // let list2$ = cheerio.load(listRes2.data)
+  // blogList = list2$('.article-item-box')
+  let listRes3 = await axios.get(listUrl3)
+  // let list3$ = cheerio.load(listRes3.data)
+  let list$ = cheerio.load(res.data + listRes2.data + listRes3.data)
+  let blogList = list$('.article-item-box')
   blogNum = blogList.length
   let promises = []
   blogList.each((i, item, arr) => {
-    let url = rootUrl + list$(item).find('h3 a')[0].attribs.href
+    let url = list$(item).find('h4 a')[0].attribs.href
     promises[i] = axios.get(url).then(res => res.data).then(r => {
       let $ = cheerio.load(r)
-      let title = $('h1.csdn_top').text().trim()
+      let title = $('h6.title-article').text().trim()
       console.log(`我已经来到-${title}`)
       successNum++
     }).catch((error) => {
@@ -71,7 +75,7 @@ async function start() {
     return axios.get(stratUrl)
   }).then(res => {
     let $ = cheerio.load(res.data)
-    endViews = $('.gradeAndbadge .num')[0].children[0].data
+    endViews = $('.grade-box dd')[1].attribs.title
     console.log(`---------------------------------------`)
     console.log(`成功访问量：${successNum}`)
     console.log(`失败访问量：${errNum}`)
--- s.py
+++ s.py
@@ -5,34 +5,52 @@ from bs4 import BeautifulSoup
 
 def start():
     ''' start def '''
-    root_url = 'http://blog.csdn.net'
     strat_url = 'http://blog.csdn.net/sirm2z'
-    list_params = {'pageindex': 1, 'pagesize': 100, 'categoryId':0}
-    list_url = 'http://blog.csdn.net/sirm2z/svc/getarticles'
+    list_url2 = 'https://blog.csdn.net/sirm2z/article/list/2'
+    list_url3 = 'https://blog.csdn.net/sirm2z/article/list/3'
     start_views = 0
     end_views = 0
     success_num = 0
     err_num = 0
     response = requests.get(strat_url, timeout=5)
     soup = BeautifulSoup(response.content, "html5lib")
-    start_views = soup.find("span", class_='num').string
+    # start_views = soup.find("div", class_='.grade-box').string
+    start_views = soup.select("div.grade-box dl:nth-of-type(2) dd")[0]['title']
     print('当前访问量：%s' % start_views)
     print('---------------------------------------')
-    list_re = requests.get(list_url, params=list_params, timeout=5)
-    list_soup = BeautifulSoup(list_re.content, "html5lib")
-    list_li = list_soup.find_all('li', class_='blog-unit')
-    for item in list_li:
-        url = root_url + item.find('h3').find('a').attrs['href']
+    list_li1 = soup.find_all('div', class_='article-item-box')
+    list_re2 = requests.get(list_url2, timeout=5)
+    list_soup2 = BeautifulSoup(list_re2.content, "html5lib")
+    list_li2 = list_soup2.find_all('div', class_='article-item-box')
+    list_re3 = requests.get(list_url3, timeout=5)
+    list_soup3 = BeautifulSoup(list_re3.content, "html5lib")
+    list_li3 = list_soup3.find_all('div', class_='article-item-box')
+    for item in list_li1:
+        url = item.find('h4').find('a').attrs['href']
         item_re = requests.get(url, timeout=5)
         item_soup = BeautifulSoup(item_re.content, "html5lib")
-        title = item_soup.find('h1', class_='csdn_top').string
+        title = item_soup.find('h6', class_='title-article').string
+        print('我已经来到-%s' % title)
+        success_num += 1
+    for item in list_li2:
+        url = item.find('h4').find('a').attrs['href']
+        item_re = requests.get(url, timeout=5)
+        item_soup = BeautifulSoup(item_re.content, "html5lib")
+        title = item_soup.find('h6', class_='title-article').string
+        print('我已经来到-%s' % title)
+        success_num += 1
+    for item in list_li3:
+        url = item.find('h4').find('a').attrs['href']
+        item_re = requests.get(url, timeout=5)
+        item_soup = BeautifulSoup(item_re.content, "html5lib")
+        title = item_soup.find('h6', class_='title-article').string
         print('我已经来到-%s' % title)
         success_num += 1
     result = requests.get(strat_url, timeout=5)
     result_soup = BeautifulSoup(result.content, "html5lib")
-    end_views = result_soup.find("span", class_='num').string
+    end_views = result_soup.select("div.grade-box dl:nth-of-type(2) dd")[0]['title']
     print('---------------------------------------')
-    print('文章总数目：%s' % len(list_li))
+    print('文章总数目：%s' % (len(list_li1) + len(list_li2) + len(list_li3)))
     print('成功访问量：%s' % success_num)
     print('失败访问量：%s' % err_num)
     print('当前访问量：%s' % end_views)
